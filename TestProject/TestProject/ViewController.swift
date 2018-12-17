5b28d7c7dd9a46b80282d13a5e3d65dfa9a0c32c
Sergey Pogrebnyak <sergey.pogrebnuak@nixsolutions.com>
2018-12-17 16:08:42 +0200

    start project + git ignore

---
+++ TestProject/TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Sergey Pogrebnyak on 12/17/18.
+//  Copyright © 2018 Sergey Pogrebnyak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
