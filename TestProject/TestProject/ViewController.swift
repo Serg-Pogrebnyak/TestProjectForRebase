e8e007200ca9a49aeed5f6e9564c30c59cff0ca9
Sergey Pogrebnyak <sergey.pogrebnuak@nixsolutions.com>
2018-12-17 16:50:26 +0200

    part 4
    commit 4
    
    commit 5
    
    commit 6
    
    this is a sqosh

--- TestProject/TestProject/ViewController.swift
+++ TestProject/TestProject/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
         //part 1
         //part 1 1
         //part 1 1 1
+        //part 1 1 1 1
+        //part 1 1 1 1 1
+        //part 1 1 1 1 1 1
     }
 
 
